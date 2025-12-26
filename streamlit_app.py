890d17528bfbf82528fc958db6504ceb37d4c5a3
guyrl9271-coder <guyrl9271@gmail.com>
2025-12-26 14:08:38 +0900

    initial streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="🩸 Villain Archetype Analyzer",
+    page_icon="🕯️",
+    layout="centered"
+)
+
+st.title("🩸 Villain Archetype Analyzer")
+st.write("정상 작동 테스트")
